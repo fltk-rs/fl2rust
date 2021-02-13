4b088da6f89e8d715969fcd46dd259e275b5136f
MoAlyousef <may642_2000@hotmail.com>
2021-02-14 00:06:24 +0300

    add example

---
+++ examples/example.rs
@@ -0,0 +1,10 @@
+use fltk::*;
+
+#[path = "myuifile.rs"]
+mod myuifile;
+
+fn main() {
+    let app = app::App::default();
+    let _ui = myuifile::UserInterface::make_window();
+    app.run().unwrap();
+}
